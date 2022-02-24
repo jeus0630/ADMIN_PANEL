94e1c75cdb3f20956e0625c869db516f61a31478
Jewoo <jeus0630@designfever.com>
2022-02-25 01:46:58 +0900

    Style: topbar.scss, Topbar.tsx
    topbar styling

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import "./topbar.scss";
+import { NotificationsNone, Language, Settings } from '@mui/icons-material';
 
 interface ITopbarProps {
 }
@@ -9,10 +10,23 @@ const Topbar: React.FunctionComponent<ITopbarProps> = (props) => {
         <div className="top-bar">
             <div className="top-bar-wrapper">
                 <div className="top-left">
-                    Left
+                    <span className="logo">JewooAdmin</span>
                 </div>
                 <div className="top-right">
-                    Right
+                    <div className="top-bar-icon-container">
+                        <NotificationsNone></NotificationsNone>
+                        <span className="top-icon-badge">2</span>
+                    </div>
+                    <div className="top-bar-icon-container">
+                        <Language></Language>
+                        <span className="top-icon-badge">2</span>
+                    </div>
+                    <div className="top-bar-icon-container">
+                        <Settings></Settings>
+                    </div>
+                    <div className="img-wrap">
+                        <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="top-avatar" />
+                    </div>
                 </div>
             </div>
         </div>
