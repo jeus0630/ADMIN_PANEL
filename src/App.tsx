4bd6dc085df8975924cfad9f0a3f78c3f11c2ad8
Jewoo <jeus0630@designfever.com>
2022-02-26 00:12:01 +0900

    Docs: Products.tsx
    Products 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
+import Products from './pages/products/Products';
 
 function App() {
   return (
@@ -20,7 +21,9 @@ function App() {
             <Route path="/users" element={<UserList></UserList>}></Route>
             <Route path="/user/:id" element={<User></User>}></Route>
             <Route path="/new-user" element={<NewUser></NewUser>}></Route>
-            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/products" element={<Products></Products>}></Route>
+            <Route path="/product/:id" element={<User></User>}></Route>
+            <Route path="/new-product" element={<NewUser></NewUser>}></Route>
           </Routes>
         </div>
       </div>
